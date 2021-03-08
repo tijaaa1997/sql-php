f41c5a32b0abfa7c0262126a4f095d454e09d0ee
Milos Tijanic <mikigrobar1997@gmail.com>
2021-03-08 21:11:01 +0100

    konekcoja ka bazi

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$servername = "127.0.0.1";
+$username = "milos";
+$password = "milos";
+$dbname = "users_db";
+
+try{
+    $connection = new POD ("mysql : host=$servername";dbname=$dbname",$username,$password);
+    
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE
+
+    
+}
